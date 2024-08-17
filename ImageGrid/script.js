b58a0343f981deb28b5e452fb055c822f34294c0
SurajKumar <srjkumar9891@gmail.com>
2024-08-17 22:13:23 +0530

    image grid

---
+++ ImageGrid/script.js
@@ -0,0 +1,11 @@
+let imgContainer = document.querySelector(".imgContainer")
+
+function oneColumn(){
+    imgContainer.style.columns = 1
+}
+function twoColumn(){
+    imgContainer.style.columns = 2
+}
+function threeColumn(){
+    imgContainer.style.columns = 3
+}
